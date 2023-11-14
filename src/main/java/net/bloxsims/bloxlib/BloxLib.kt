8bb357e9a592ec1f488a6a31176cf4b333950d9a
TheCodingDuck <71715967+colton-boi@users.noreply.github.com>
2023-11-14 16:59:47 -0500

    Oops (fix errors)

--- src/main/java/net/bloxsims/bloxlib/BloxLib.kt
+++ src/main/java/net/bloxsims/bloxlib/BloxLib.kt
@@ -1,10 +1,6 @@
 package net.bloxsims.bloxlib
 
-import net.bloxsims.bloxlib.displays.ChunkListener
-import net.bloxsims.bloxlib.displays.Display
 import org.apache.maven.artifact.versioning.ComparableVersion
-import org.bukkit.Bukkit
-import org.bukkit.Chunk
 import org.bukkit.plugin.java.JavaPlugin
 
 class BloxLib {
@@ -15,12 +11,9 @@ class BloxLib {
         private lateinit var plugin: JavaPlugin
         val TRUE_VALUES: List<String> = listOf("true", "yes", "on", "enable", "enabled")
         val FALSE_VALUES: List<String> = listOf("false", "no", "off", "disable", "disabled")
-        val DISPLAY_ENTITIES: HashMap<Chunk, MutableList<Display>> = HashMap()
 
         fun setPlugin(plugin: JavaPlugin) {
             this.plugin = plugin
-
-            Bukkit.getPluginManager().registerEvents(ChunkListener(), plugin)
         }
 
         fun getPlugin(): JavaPlugin {
