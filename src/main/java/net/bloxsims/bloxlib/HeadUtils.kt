77d4ceb913fb57ec8eb52eaeba18c7f34ff0d7c3
TheCodingDuck <71715967+colton-boi@users.noreply.github.com>
2023-11-14 20:30:44 -0500

    Fix heads

--- src/main/java/net/bloxsims/bloxlib/HeadUtils.kt
+++ src/main/java/net/bloxsims/bloxlib/HeadUtils.kt
@@ -1,5 +1,6 @@
 package net.bloxsims.bloxlib
 
+import com.destroystokyo.paper.profile.CraftPlayerProfile
 import com.mojang.authlib.GameProfile
 import com.mojang.authlib.properties.Property
 import org.bukkit.inventory.ItemStack
@@ -7,19 +8,17 @@ import org.bukkit.inventory.meta.SkullMeta
 import java.util.*
 
 fun ItemStack.texture(texture: String) {
-    if (itemMeta as? SkullMeta != null) {
+    val skullMeta = this.itemMeta as? SkullMeta
+    if (skullMeta != null) {
+        val metaSetProfileMethod = (skullMeta).javaClass.getDeclaredMethod("setProfile", GameProfile::class.java)
+        metaSetProfileMethod.trySetAccessible()
         val id = UUID(
             texture.substring(texture.length - 20).hashCode().toLong(),
             texture.substring(texture.length - 10).hashCode().toLong()
         )
-        val profile = GameProfile(id, "Player").apply {
-            properties.put("textures", Property("textures", texture))
-        }
-        (itemMeta as SkullMeta).invokeMethod(
-            name = "setProfile",
-            parameterTypes = arrayOf((GameProfile::class.java)),
-            obj = profile
-        )
-        this.setItemMeta(itemMeta)
+        val profile = GameProfile(id, "Player")
+        profile.properties.put("textures", Property("textures", texture))
+        metaSetProfileMethod.invoke(skullMeta, profile)
+        this.setItemMeta(skullMeta)
     }
 }
